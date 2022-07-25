37bdc35246dca82430335750124718821b691111
ArthurKorchak <korchak.arthur@gmail.com>
2022-07-25 15:10:29 +0300

    upd

---
+++ index-module-4.js
@@ -0,0 +1,14 @@
+// const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'David', 'Roman', 'Olga'];
+// const mens = ['Artem', 'Maksim', 'David', 'Roman'];
+// const womens = []
+
+// users.forEach(user => !mens.includes(user) ? womens.push(user) : '');
+// console.log(womens);
+
+
+// users.forEach((user, index, array) => [mens.includes(user) ? array[index] = 0 : null]);
+// const a = [...new Set(users)].join(' ').replace('0 ', '').split(' ');
+// console.log(a);
+
+// ---------------------------------------------------------------------------------------------------
+
