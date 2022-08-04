98e936206e6c8923a2b8041b7bf65c09f09b4fdf
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-04 16:47:51 +0300

    upd

---
+++ index-module-7.js
@@ -0,0 +1,47 @@
+const instruments = [{
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_141546.jpg',
+    name: 'Молоток',
+    price: 150
+}, {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg',
+    name: 'Перфоратор',
+    price: 3000
+    }, {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg',
+    name: 'Рівень',
+    price: 2000
+}];
+
+elemGen(instruments);
+
+window.addEventListener("click", (event) => {
+    const id = Number(event.target.parentNode.id);
+    instruments.forEach((elem) => {
+        if (elem.id === id) {
+            console.log(elem.name);
+            console.log(elem.price);
+        };
+    });
+});
+
+function elemGen(instruments) {
+    const addPlace = document.querySelector("body");
+    const gen = instruments.reduce((acc, {id, img, name, price}) => {
+        return acc += 
+            `<div class="card" id="${id}">
+                <img src="${img}" width="250"></img>
+                <div class="foot">
+                    <p class="name">${name}</p>
+                    <p class="price">${price} кредитів</p>
+                </div>
+                <button>Купити</button>
+            </div>`
+    }, ``);
+    addPlace.insertAdjacentHTML("afterbegin", gen);
+};
+
+// --------------------------------------------------------------------------------------------------- 01
+
